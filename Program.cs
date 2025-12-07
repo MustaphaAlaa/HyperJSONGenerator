6d5cbb06e2437df9ee21cf6e1750636b5c5d6bfe
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-12-07 02:00:25 +0200

    Change the number of years to 20

--- Program.cs
+++ Program.cs
@@ -12,18 +12,22 @@ internal class Program
 
     static readonly string _googleAnalyticsFileName = Path.Combine(projectRoot, "GA-mock.json");
     static readonly string _pageSpeedInsightFileName = Path.Combine(projectRoot, "PSI-mock.json");
-
+        
     private static async Task Main(string[] args)
-    {
+    { 
+
         System.Console.WriteLine("Start generating  json files.....");
+    
         var stopwatch = new Stopwatch();
         stopwatch.Start();
 
         int days = 365;
-        int years = 1850;
+        int years = 20;
         int hours = 24;
 
+        //the number of records for each json file will be duration * number of pages (23)
         int duration = days * years * hours;
+
 
         var GoogleAnalyticsTask = JsonsGenerator.Generate<GoogleAnalytics>(duration, _googleAnalyticsFileName);
         var PageSpeedInsightTask =
